fda2c40a4e8512c0215575c0795604bef5748846
Stu Small <stuart.alan.small@gmail.com>
2024-10-13 07:00:56 -0600

    fmt

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,11 +1,14 @@
 import _OnBeforeRequestDetails = browser.webRequest._OnBeforeRequestDetails;
 
 function onBeforeRequest(details: _OnBeforeRequestDetails) {
-console.log(details)
+  console.log(details);
 }
 
-browser.webRequest.onBeforeRequest.addListener(onBeforeRequest, {
+browser.webRequest.onBeforeRequest.addListener(
+  onBeforeRequest,
+  {
     urls: ["<all_urls>"],
-    types: ["main_frame"]},
-    ["blocking"]
+    types: ["main_frame"],
+  },
+  ["blocking"],
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: {
-        background: "./src/background/background.ts"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        static: path.join(__dirname, "dist"),
-        compress: true,
-        port: 4000,
-    },
+  entry: {
+    background: "./src/background/background.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4000,
+  },
 };
