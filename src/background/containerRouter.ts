c875d7129a723b7e24d329b1800b0d71981de8a4
Stu Small <stuart.alan.small@gmail.com>
2024-11-26 16:25:31 -0700

    Various bug fixes

--- src/background/containerRouter.ts
+++ src/background/containerRouter.ts
@@ -28,9 +28,26 @@ export class ContainerRouter {
       this.lookup[site] = contextualIdentity.cookieStoreId;
     }
   }
+  clear() {
+    this.lookup = {};
+  }
 
   lookUpHostname(hostname: URL): string {
     console.debug("Looking up " + hostname.hostname);
-    return this.lookup[hostname.hostname];
+    console.debug("Contents is " + JSON.stringify(this.lookup));
+    const split = hostname.hostname.split(".");
+
+    if (split.length >= 2) {
+      const last = split.pop();
+      const base = split.pop();
+      const result = this.lookup[base + "." + last];
+      if (result == undefined) {
+        return "firefox-default";
+      } else {
+        return result;
+      }
+    } else {
+      return "firefox-default";
+    }
   }
 }
